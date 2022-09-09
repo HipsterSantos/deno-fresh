daf5c4a871896664aa0c1a9becccf4538dcb5ebc
santosferreira <santoscampos269@gmail.com>
2022-09-09 07:00:08 +0100

    would be impossible that test passed if it's doing defferent ad it was programmed to do and expetecd to show up

--- test.ts
+++ test.ts
@@ -75,7 +75,7 @@ Deno.test("E2E test", async (t) => {
     const button = await page.querySelector(".button");
     await button.click({ waitFor: "navigation" });
 
-    assertEquals(await page.location(), `${remote}jobs/${name}`);
+    assertEquals(await page.location(), `${remote}jobs/allsearch.php?freeword=${name}&searchbtn=SEARCH+JOB`);
 
     const body = await page.evaluate(() => {
       return document.querySelector("div")?.innerText;
